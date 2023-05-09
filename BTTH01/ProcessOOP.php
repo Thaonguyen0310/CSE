938ca41cb6359d3497deeecfc7137df7007df099
thaonguyen0310 <2051060722@e.tlu.edu.vn>
2023-05-09 22:08:26 +0700

    update

--- BTTH01/ProcessOOP.php
+++ BTTH01/ProcessOOP.php
@@ -1,76 +1,92 @@
 <?php
 class Student {
-    private $id;
-    private $name;
-    private $age;
-
-    public function __construct($id, $name, $age) {
-        $this->id = $id;
-        $this->name = $name;
-        $this->age = $age;
-    }
-
-    public function getId() {
-        return $this->id;
-    }
-
-    public function setId($id) {
-        $this->id = $id;
-    }
-
-    public function getName() {
-        return $this->name;
-    }
-
-    public function setName($name) {
-        $this->name = $name;
-    }
-
-    public function getAge() {
-        return $this->age;
-    }
-
-    public function setAge($age) {
-        $this->age = $age;
-    }
+  private $id;
+  private $name;
+  private $age;
+  private $grade;
+  
+  function __construct($id, $name, $age, $grade) {
+    $this->id = $id;
+    $this->name = $name;
+    $this->age = $age;
+    $this->grade = $grade;
+  }
+  
+  public function getId() {
+    return $this->id;
+  }
+  
+  public function getName() {
+    return $this->name;
+  }
+  
+  public function getAge() {
+    return $this->age;
+  }
+  
+  public function getGrade() {
+    return $this->grade;
+  }
+  
+  public function setId($id) {
+    $this->id = $id;
+  }
+  
+  public function setName($name) {
+    $this->name = $name;
+  }
+  
+  public function setAge($age) {
+    $this->age = $age;
+  }
+  
+  public function setGrade($grade) {
+    $this->grade = $grade;
+  }
 }
+
 class StudentDAO {
-    private $students = array();
-
-    public function create($student) {
-        array_push($this->students, $student);
+  private $students;
+  
+  function __construct() {
+    $this->students = array();
+  }
+  
+  public function create($student) {
+    array_push($this->students, $student);
+  }
+  
+  public function read($id) {
+    foreach($this->students as $student) {
+      if($student->getId() == $id) {
+        return $student;
+      }
     }
-
-    public function read($id) {
-        foreach ($this->students as $student) {
-            if ($student->getId() == $id) {
-                return $student;
-            }
-        }
-        return null;
+    return null;
+  }
+  
+  public function update($student) {
+    foreach($this->students as &$s) {
+      if($s->getId() == $student->getId()) {
+        $s = $student;
+        return true;
+      }
     }
-
-    public function update($student) {
-        for ($i = 0; $i < count($this->students); $i++) {
-            if ($this->students[$i]->getId() == $student->getId()) {
-                $this->students[$i] = $student;
-                return;
-            }
-        }
+    return false;
+  }
+  
+  public function delete($id) {
+    foreach($this->students as $key => $student) {
+      if($student->getId() == $id) {
+        unset($this->students[$key]);
+        return true;
+      }
     }
-
-    public function delete($id) {
-        for ($i = 0; $i < count($this->students); $i++) {
-            if ($this->students[$i]->getId() == $id) {
-                unset($this->students[$i]);
-                $this->students = array_values($this->students);
-                return;
-            }
-        }
-    }
-
-    public function getAll() {
-        return $this->students;
-    }
+    return false;
+  }
+  
+  public function getAll() {
+    return $this->students;
+  }
 }
 ?>
